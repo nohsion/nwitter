97d61f021067627d75f84312b33c88ee59715e13
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-25 20:21:08 +0900

    create read 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,51 @@
-const Home = () => <span>Home</span>
+import { useEffect, useState } from "react"
+import { dbService } from "fbase"
+
+
+const Home = () => {
+    const [nweet, setNweet] = useState("")
+    const [nweets, setNweets] = useState([])
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        await dbService.collection("nweets").add({
+            text: nweet,
+            createdAt: Date.now(),
+        })
+        setNweet("")
+    }
+
+    const onChange = (event) => {
+        event.preventDefault()
+        const {
+            target: { value },
+        } = event
+        setNweet(value)
+    }
+
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get()
+        dbNweets.forEach((document) => {
+            setNweets((prev) => [document.data(), ...prev])
+        })
+    }
+
+    useEffect(() => {
+        getNweets()
+    }, [])
+    console.log(nweets)
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                value={nweet}
+                onChange={onChange}
+                type="text"
+                placeholder="What's on your mind?"
+                maxLength={128}
+            />
+            <input type="submit" value="Nweet" />
+        </form>
+    )
+}
 
 export default Home
