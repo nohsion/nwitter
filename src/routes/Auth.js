b6ab3b8c3fca1c1c0d25f35460c48e537d769ed0
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-25 19:30:27 +0900

    로그인 회원가입  토글버튼

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,12 @@
 import { useState } from "react"
+import { authService } from "fbase"
 
 
 const Auth = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [newAccount, setNewAccount] = useState(true)
+    const [error, setError] = useState("")
 
     const onChange = (event) => {
         const {
@@ -17,9 +20,25 @@ const Auth = () => {
         }
     }
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault()
+        try {
+            let data
+            if (newAccount) {
+                // create newAccount
+                data = await authService.createUserWithEmailAndPassword(email, password)
+            }
+            else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password)
+            }
+            console.log(data)
+        } catch (error) {
+            setError(error.message)
+        }
     }
+
+    const toggleAccount = () => setNewAccount((prev) => !prev)
 
     return (
         <div>
@@ -40,8 +59,12 @@ const Auth = () => {
                     value={password}
                     onChange={onChange}
                 />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign in" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
