a84ed0dc4fe3ac5ee486b4f837ace1d0dfff5a03
Sion Noh <shtldhs123@ajou.ac.kr>
2021-09-04 17:43:23 +0900

    로그아웃 실시간

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import { authService } from "fbase"
 
 function App() {
   const [init, setInit] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [userObj, setUserObj] = useState(null)
 
   useEffect(() => {
@@ -17,7 +16,7 @@ function App() {
         })
       }
       else {
-        setIsLoggedIn(false)
+        setUserObj(false)
       }
       setInit(true)
     })
