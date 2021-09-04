2491788d262350f48798f04a4381b8195630b116
Sion Noh <shtldhs123@ajou.ac.kr>
2021-09-04 17:18:08 +0900

    Home 컴포넌트 나누기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,57 +1,12 @@
 import { useEffect, useState } from "react"
-import { dbService, storageService } from "fbase"
+import { dbService } from "fbase"
 import Nweet from "components/Nweet"
-import { v4 as uuidv4 } from "uuid"
+import NweetFactory from "components/NweetFactory"
+
 
 const Home = ({ userObj }) => {
-    const [nweet, setNweet] = useState("")
     const [nweets, setNweets] = useState([])
-    const [attachment, setAttachment] = useState("")
-
-    const onSubmit = async (event) => {
-        event.preventDefault()
-        let attachmentUrl = ""
-        if (attachment !== "") {
-            const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`)
-            const response = await attachmentRef.putString(attachment, "data_url")
-            attachmentUrl = await response.ref.getDownloadURL()
-        }
-
-        await dbService.collection("nweets").add({
-            text: nweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-            attachmentUrl,
-        })
-        setNweet("")
-        setAttachment("")
-    }
-
-    const onChange = (event) => {
-        event.preventDefault()
-        const {
-            target: { value },
-        } = event
-        setNweet(value)
-    }
-
-    const onFileChange = (event) => {
-        const {
-            target: { files }
-        } = event
-        const theFile = files[0]
-        const reader = new FileReader()
-        reader.onloadend = (finishedEvent) => {
-            const {
-                currentTarget: { result }
-            } = finishedEvent
-            setAttachment(result)
-        }
-        reader.readAsDataURL(theFile)
-    }
-
-    const onClearAttachment = () => setAttachment("")
-
+    
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) => {
             const newArray = snapshot.docs.map((document) => ({
@@ -65,23 +20,7 @@ const Home = ({ userObj }) => {
 
     return (
         <>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={nweet}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="What's on your mind?"
-                    maxLength={128}
-                />
-                <input type="file" accept="image/*" onChange={onFileChange} />
-                <input type="submit" value="Nweet" />
-                {attachment && (
-                    <div>
-                        <img src={attachment} width="50px" height="50px" />
-                        <button onClick={onClearAttachment}>Clear</button>
-                    </div>
-                )}
-            </form>
+            <NweetFactory userObj={userObj} />
             <div>
                 {nweets.map((nweet) => (
                     <Nweet
