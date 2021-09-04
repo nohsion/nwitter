e817c821608687d5a2338046f19c8a45e6b2eb1f
Sion Noh <shtldhs123@ajou.ac.kr>
2021-09-04 17:01:29 +0900

    프로필 사용자 이름 변경하기 (default - oauth2 name)

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,30 +1,38 @@
 import { authService, dbService } from "fbase"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import { useHistory } from "react-router"
 
 
 const Profile = ({ userObj }) => {
     const history = useHistory()
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
 
     const onLogOutClick = () => {
         authService.signOut()
         history.push("/")
     }
 
-    const getMyNweets = async () => {
-        const nweets = await dbService.collection("nweets")
-            .where("creatorId", "==", userObj.uid)
-            .orderBy("createdAt", "asc")
-            .get()
-        console.log(nweets.docs.map((doc) => doc.data()))
+    const onChange = (event) => {
+        const {
+            target: { value }
+        } = event
+        setNewDisplayName(value)
     }
 
-    useEffect(() => {
-        getMyNweets()
-    }, [])
-
+    const onSubmit = (event) => {
+        event.preventDefault()
+    }
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Display name"
+                    value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     )
